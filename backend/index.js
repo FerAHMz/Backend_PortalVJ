87308e92dd90ae04a027afc0be7536c779202100
FelipeAP04 <agu23195@uvg.edu.gt>
2025-04-05 22:30:15 -0600

    log in por usuarios

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,16 @@ app.use(express.json()); // Para leer JSON del frontend
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
-  try { //login para los padres por el momento
+  try {
     const result = await db.getPool().query(
-      'SELECT * FROM padres WHERE email = $1 AND password = $2',
+      `SELECT u.rol FROM usuarios u
+       JOIN padres p ON u.id = p.rol AND p.email = $1 AND p.password = $2
+       UNION
+       SELECT u.rol FROM usuarios u
+       JOIN maestros m ON u.id = m.rol AND m.email = $1 AND m.password = $2
+       UNION
+       SELECT u.rol FROM usuarios u
+       JOIN administrativos a ON u.id = a.rol AND a.email = $1 AND a.password = $2`,
       [email, password]
     );
 
