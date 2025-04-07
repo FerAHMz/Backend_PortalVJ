ee53bd39290a0d74fb0a3c704bebcc5221525ab2
Fernando Hernandez <ferandree270904@gmail.com>
2025-04-06 21:55:37 -0600

    Update hashed password process

---
+++ DB/sql/04_hash_passwords.sql
@@ -0,0 +1,14 @@
+SELECT pg_sleep(2);
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+SELECT pg_sleep(1);
+
+UPDATE maestros 
+SET password = crypt(CONCAT('password', id::text), gen_salt('bf', 10));
+
+UPDATE padres 
+SET password = crypt(CONCAT('password', id::text), gen_salt('bf', 10));
+
+UPDATE administrativos 
+SET password = crypt(CONCAT('password', id::text), gen_salt('bf', 10));
